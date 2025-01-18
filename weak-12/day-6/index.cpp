cf312c0d99ba6bb4dadab554f0132fcb49893df8
zanit-solution <zanltd.bd@gmail.com>
2025-01-19 00:12:38 +0600

    sdfds

--- weak-12/day-6/index.cpp
+++ weak-12/day-6/index.cpp
@@ -1,30 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
 
    int n, q;
    cin >> n >> q;
    vector<int> a(n);
-   for (int i = 0;i < n;i++) {
+   for (int i = 0; i < n; i++)
+   {
       cin >> a[i];
    }
 
-   for (int i = 0;i < q;i++) {
+   for (int i = 0; i < q; i++)
+   {
       int key, l = 0, r = n - 1, mid, ans = -1;
       cin >> key;
-      while (l <= r) {
+      while (l <= r)
+      {
          mid = l + (r - l) / 2;
-         if (key == a[mid]) {
+         if (key == a[mid])
+         {
             ans = mid;
             r = mid - 1;
          }
-         else if (key < a[mid]) {
+         else if (key < a[mid])
+         {
             r = mid - 1;
          }
-         else {
+         else
+         {
             l = mid + 1;
          }
       }
